daf9f1820e389afbc41c53bc4f629b21966d4d7a
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2023-01-24 15:34:53 +0100

    main.py in flaskdeta - deploy changes in app

--- flaskdeta/main.py
+++ flaskdeta/main.py
@@ -5,4 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World! From Adam Jurkiewicz'
+    return 'Hello, World! From Adam Jurkiewicz - go to <a href="deploy"> deploy endpoint</a>'
+
+@app.route('/deploy')
+def hello_next():
+    return 'Hello, World! From Adam Jurkiewicz - make <pre>deta deploy</pre> to push changes.'
